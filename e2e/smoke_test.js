ea8fcff70e40c169e91222e04060bec0d1cdbf37
江本一生 <issei@emotoisshounoMacBook-Air.local>
2024-09-16 18:23:37 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,5 +1,10 @@
 SuiteOf(' スモークテスト ');
-Scenario('example.comにアクセスする', ({ I }) => { 
- I.amOnPage('https://example.com')
- I.see('Example Domain')
+
+Scenario('Webサイトを開きログインする', ({ I }) => {
+  I.amOnPage('https://fastify-webapp-sample.takuyasuemura.dev/')
+  I.click('ログインする')
+  I.fillField('ユーザー名', 'user1')
+  I.fillField('パスワード', 'super-strong-passphrase')
+  I.click('ログイン')
+  I.see('user1 さん')
 });
